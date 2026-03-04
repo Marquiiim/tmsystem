3d5ed35f6a5f849531cb1dfb4378c242ad9b34d1
Marcos Gabriel <marcosindev@gmail.com>
2026-03-04 19:06:24 -0300

    Update database.js

--- tmsystem/api/src/config/database.js
+++ tmsystem/api/src/config/database.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
     port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0,
+    queueLimit: 0
 });
 
 const query = (sql, params) => {
